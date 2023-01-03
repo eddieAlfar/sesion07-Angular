844cd35d24e984a5d7eec01aeec7039e57b0adbe
eddieAlfaro <edbie2415@hotmail.com>
2023-01-02 21:06:09 -0500

    first commit

--- src/app/home/calculator/calculator.component.ts
+++ src/app/home/calculator/calculator.component.ts
@@ -9,11 +9,38 @@ export class CalculatorComponent {
 
   num1:number=0;
   num2:number=0;
+  operacion: string = 'defecto';
   resultado:number=0;
 
+ 
   onSumar():void{
     this.resultado=this.num1+this.num2;
   }
+  onRestar():void{
+    this.resultado=this.num2-this.num1;
+  }
+  onMulti():void{
+    this.resultado=this.num1*this.num2;
+  }
+  onDiv():void{
+    this.resultado=this.num1/this.num2;
+  }
+ 
+  calcular(): number {
+    if (this.operacion === 'sumar') {
+      this.onSumar();
+      return this.resultado;
+    } else if (this.operacion === 'restar') {
+      this.onRestar();
+      return this.resultado;
+    } else if (this.operacion === 'multiplicar') {
+      this.onMulti();
+      return this.resultado;
+    } else if (this.operacion === 'dividir') {
+      this.onDiv();
+      return this.resultado;
+    }
 
-
+    return 0;
+  }
 }
